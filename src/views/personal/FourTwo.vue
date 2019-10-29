3c7dc5e406751a1462cdc70d3625d1be3fe95075
osada <osamasa@gmail.com>
2019-10-29 10:43:53 +0900

    add firebase

--- src/views/personal/FourTwo.vue
+++ src/views/personal/FourTwo.vue
@@ -4,7 +4,7 @@
       <router-link to="/">トップ</router-link> ＞<router-link to="/personal">社員用トップ</router-link>＞<router-link to="/personal/four">状態検知と改善</router-link>＞<router-link to="/personal/fourtwo">家族と共有</router-link>
       <h1>家族と共有</h1>
       <div id="qrcodeview">
-      <img src="../../assets/qr20191025105848968.png">
+      <img src="../../assets/ab801e8fc0c16e39d3b2328be165c57e.png">
       </div>
       <div id="sendfamily">
       <div class="row justify-content-center">
