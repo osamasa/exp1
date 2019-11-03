55de9f2a91bd5c0648223159de99b1a278d169c0
osada <osamasa@gmail.com>
2019-11-03 09:11:36 +0900

    add coments

--- src/views/company/One.vue
+++ src/views/company/One.vue
@@ -8,8 +8,8 @@
 <div id="jinjimenu" class="container-fluid">
 <div class="row row-eq-height vuetilemenu">
   <div class="col-md-4"><router-link type="button" class="btn btn-primary btn-lg btn-block" to="/company/oneone/5/3/企業への信頼性と職場環境/過労察知/居心地/肉体疲労/*恐怖を感じる/*働く喜び/*宿場の不安/*やらされ感/*チャンレンジ精神">企業への信頼性と<br/>職場環境<br/>5/<font color="lightpink">3</font></router-link></div>
-  <div class="col-md-4 "><router-link type="button" class="btn btn-primary btn-lg btn-block" to="/company/oneone">会議の<br/>最小限化<br/>4/<font color="lightpink">4</font></router-link></div>
-  <div class="col-md-4 "><router-link type="button" class="btn btn-primary btn-lg btn-block" to="/company/oneone">働く場所の<br/>最適化<br/>3/<font color="lightpink">2</font></router-link></div>
+  <div class="col-md-4 "><router-link type="button" class="btn btn-primary btn-lg btn-block" to="/company/oneone/4/4/会議の最小限化/*会議不参加/*参加型会議/*長時間会議/*緊張感/前向きな意見/理不尽/不参加希望/達成感">会議の<br/>最小限化<br/>4/<font color="lightpink">4</font></router-link></div>
+  <div class="col-md-4 "><router-link type="button" class="btn btn-primary btn-lg btn-block" to="/company/oneone/3/2/働く場所の最適化/規律/レスポンス/*落ち着き度/*イライラ/*安堵感/うんざり感/優うつ度/有意義">働く場所の<br/>最適化<br/>3/<font color="lightpink">2</font></router-link></div>
 </div>
 
 <div class="row row-eq-height vuetilemenu">
